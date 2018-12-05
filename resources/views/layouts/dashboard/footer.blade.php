e6fd14b49a977921c310eb6b1f9a8a25b4a78bb4
emmanuel steve <emmanuel steve>
2018-12-05 14:53:31 +0100

    from talk2coded

--- resources/views/layouts/dashboard/footer.blade.php
+++ resources/views/layouts/dashboard/footer.blade.php
@@ -1,13 +1,18 @@
 
- <footer class="foooter">
- 
-      
- <div class="container-fluid">
+ <footer class="foooter d-none d-md-block  md-footer ">     
     <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
      <a class="fbport" href="#" target="_blank">fbportal</a>. All rights reserved.</span>
         <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">made with
           <i class="mdi mdi-heart text-danger"></i> at Innovation growth hub Aba
     </span>
-  </div> 
+</footer>
+
+<footer class="foooter d-block d-sm-none sm-footer">     
+    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
+     <a class="fbport" href="#" target="_blank">fbportal</a>. All rights reserved.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">made with
+          <i class="mdi mdi-heart text-danger"></i> at Innovation growth hub Aba
+    </span>
+</footer>
 
 
